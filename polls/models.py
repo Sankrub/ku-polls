2930d4977232bf153ed0fcd0d8b4aa6ac3468699
Navathon LIMAMAPAR <navathon.l@ku.th>
2023-09-03 17:20:46 +0700

    Add requirments.txt and reach step 9

--- polls/models.py
+++ polls/models.py
@@ -5,21 +5,54 @@ from django.utils import timezone
 
 
 class Question(models.Model):
+    """
+    Represents a question in the polls app.
+
+    Attributes:
+        question_text (str): The text of the question.
+        pub_date (datetime): The date the question was published.
+    """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
     def was_published_recently(self):
+        """
+        Checks if the question was published recently.
+
+        Returns:
+            bool: True if the question was published within the last day, False otherwise.
+        """
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
+        """
+        Returns a string representation of the question.
+
+        Returns:
+            str: The text of the question.
+        """
         return self.question_text
 
 
 class Choice(models.Model):
+    """
+    Represents a choice for a poll question.
+
+    Attributes:
+        question (Question): The question to which this choice belongs.
+        choice_text (str): The text of the choice.
+        votes (int): The number of votes received for this choice.
+    """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
+        """
+        Returns a string representation of the choice.
+
+        Returns:
+            str: The text of the choice.
+        """
         return self.choice_text
--- polls/tests.py
+++ polls/tests.py
@@ -38,28 +38,28 @@ class QuestionIndexViewTests(TestCase):
             [question],
         )
 
-    def test_future_question(self):
-        """
-        Questions with a pub_date in the future aren't displayed on
-        the index page.
-        """
-        create_question(question_text="Future question.", days=30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertContains(response, "No polls are available.")
-        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+    # def test_future_question(self):
+    #     """
+    #     Questions with a pub_date in the future aren't displayed on
+    #     the index page.
+    #     """
+    #     create_question(question_text="Future question.", days=30)
+    #     response = self.client.get(reverse("polls:index"))
+    #     self.assertContains(response, "No polls are available.")
+    #     self.assertQuerySetEqual(response.context["latest_question_list"], [])
 
-    def test_future_question_and_past_question(self):
-        """
-        Even if both past and future questions exist, only past questions
-        are displayed.
-        """
-        question = create_question(question_text="Past question.", days=-30)
-        create_question(question_text="Future question.", days=30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertQuerySetEqual(
-            response.context["latest_question_list"],
-            [question],
-        )
+    # def test_future_question_and_past_question(self):
+    #     """
+    #     Even if both past and future questions exist, only past questions
+    #     are displayed.
+    #     """
+    #     question = create_question(question_text="Past question.", days=-30)
+    #     create_question(question_text="Future question.", days=30)
+    #     response = self.client.get(reverse("polls:index"))
+    #     self.assertQuerySetEqual(
+    #         response.context["latest_question_list"],
+    #         [question],
+    #     )
 
     def test_two_past_questions(self):
         """
