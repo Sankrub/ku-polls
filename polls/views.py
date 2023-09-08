f05ef27afa200a56797e9c838914c3bc0ee7615a
Navathon LIMAMAPAR <navathon.l@ku.th>
2023-09-08 21:35:21 +0700

    update view

--- polls/views.py
+++ polls/views.py
@@ -13,7 +13,8 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).exclude(
+            end_date__lt=timezone.now()).order_by("pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
@@ -24,7 +25,7 @@ class DetailView(generic.DetailView):
         """
         Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(pub_date__lte=timezone.now(),end_date__gte=timezone.now())
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
