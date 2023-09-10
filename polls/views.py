986874cc34d36a071506e76345f688faea0124f3
Navathon LIMAMAPAR <navathon.l@ku.th>
2023-09-10 19:03:07 +0700

    update

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
