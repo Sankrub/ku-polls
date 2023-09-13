27b6a0ab02e78ddf609b30affaefdbfb986f15a6
Sankrub <navathon.l@ku.th>
2023-09-14 00:45:07 +0700

    add more urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import RedirectView
 
+from . import views
+
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
     path('', RedirectView.as_view(url='/polls/')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', views.signup, name='signup'),
 ]
