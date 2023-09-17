e4212238c099603b1339e1598515165cbd36ee4f
Sankrub <navathon.l@ku.th>
2023-09-17 14:07:45 +0700

    update mysite urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login')
 ]
